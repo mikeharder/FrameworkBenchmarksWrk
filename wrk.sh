c53afa378fda91b8fd25db6381cb2db460046860
Mike Harder <mharder@microsoft.com>
2018-03-28 17:59:56 -0700

    Add parameters to use JSON or HTML accept headers

--- wrk.sh
+++ wrk.sh
@@ -3,15 +3,23 @@
 url=$1
 
 if [ "$url" = ""  ]; then
-  echo "wrk.sh url [-p] [connections]"
+  echo "wrk.sh url [-p] [-j] [-h] [connections]"
   exit 1
 fi
+
+accept="text/plain,text/html;q=0.9,application/xhtml+xml;q=0.9,application/xml;q=0.8,*/*;q=0.7"
+pipeline=
 
 if [ "$2" = "-p" ]; then
   pipeline="-s `dirname $0`/pipeline.lua -- 16"
   connections=$3
+else if [ "$2" = "-j" ]; then
+  accept="application/json,text/html;q=0.9,application/xhtml+xml;q=0.9,application/xml;q=0.8,*/*;q=0.7"
+  connections=$3
+else if [ "$2" = "-h" ]; then
+  accept="text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"
+  connections=$3
 else
-  pipeline=
   connections=$2
 fi
 
@@ -24,7 +32,7 @@ set -x
 
 wrk \
   -H 'Host: localhost' \
-  -H 'Accept: text/plain,text/html;q=0.9,application/xhtml+xml;q=0.9,application/xml;q=0.8,*/*;q=0.7' \
+  -H 'Accept: $accept' \
   -H 'Connection: keep-alive' \
   --latency \
   -d 15 \
